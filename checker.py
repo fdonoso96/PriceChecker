07b9e9736b7b01e81146719adfc58947b813971e
fdonoso <fran.donoso1996@gmail.com>
2023-03-24 15:07:11 -0500

    Added current product progress

--- checker.py
+++ checker.py
@@ -37,8 +37,9 @@ class PriceChecker:
         list_file.close()
         
     def checkPricesAmazon(self):
+        current = 1
         for url in self.urlsAmazon:
-
+            print(f"Currently checking product {current} on Amazon.")
             page = requests.get(url,headers = self.HEADERS)
             soup = bs4.BeautifulSoup(page.content,features='lxml')
             self.titlesAmazon.append(soup.find(id='productTitle').get_text().strip())
@@ -46,11 +47,15 @@ class PriceChecker:
             try:
                 #print(soup.find(id='sns-base-price').get_text().strip())
                 self.pricesAmazon.append(soup.find('span', class_='a-offscreen').get_text())
+                current = current + 1
             except:
                 self.pricesAmazon.append(" ")
+                current = current + 1
 
     def checkPricesWalmart(self):
+        current = 1
         for url in self.urlsWalmart:
+            print(f"Currently checking product {current} on Walmart.")
             profile_path = r'/Users/fdonoso/Library/Application Support/Firefox/Profiles/yjau3k6a.FrankieD'
             service = Service(r'/Users/fdonoso/Downloads/geckodriver')
             options = Options()
@@ -66,6 +71,7 @@ class PriceChecker:
                 #print(page.headers)
                 self.pricesWalmart.append(driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div/div[2]/div/section/main/div[2]/div[2]/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/span[1]/span[2]/span').text)
                 driver.quit()
+                current = current + 1
                 continue
             except:
                 print("Trying alternative XPATH")
@@ -74,9 +80,11 @@ class PriceChecker:
 
             try:
                 self.pricesWalmart.append(driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div/div[2]/div/section/main/div[2]/div[2]/div/div[1]/div/div/div[1]/div/div/div[2]/div/div/div[1]/span/span[2]/span').text)
+                current = current + 1
                 driver.quit()
             except:
                 self.pricesWalmart.append("None")
+                current = current + 1
                 driver.quit()
         
     def export(self, strMethod):
@@ -87,7 +95,7 @@ if __name__ == "__main__":
     t0 = time.time()
     
     checker = PriceChecker()
-    os.chdir("/Users/fdonoso/PycharmProjects/PriceChecker/")
+    # os.chdir("/Users/fdonoso/PycharmProjects/PriceChecker/")
     checker.loadList("AmazonList.txt")
     checker.loadList("WalmartList.txt")
     checker.checkPricesAmazon()
@@ -98,11 +106,11 @@ if __name__ == "__main__":
     
     print('\n')
     print(len(checker.urlsAmazon))
-    print(total)
+    print(total/60)
     print('\n')
 
     data = list(zip(checker.titlesAmazon,checker.pricesAmazon,checker.pricesWalmart))
-    print("(Item, Amazon Price, Walmart Price")
+    print("(Item, Amazon Price, Walmart Price)")
     print(*data,sep ="\n")
 
     # print(*checker.titlesAmazon,"-- Amazon Price: ", *checker.pricesAmazon,"-- Walmart Price: ",*checker.pricesWalmart, sep = "\n")
