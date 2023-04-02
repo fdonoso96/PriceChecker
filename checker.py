a0d4ee886bb9d238952fa28f932f8a26827f41f5
fdonoso <fran.donoso1996@gmail.com>
2023-04-02 13:16:23 -0500

    Changed scraping method to avoid amazon blocking

--- checker.py
+++ checker.py
@@ -5,6 +5,7 @@ import bs4
 import requests
 import time
 
+from selenium.common import NoSuchElementException
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -21,36 +22,59 @@ class PriceChecker:
         self.urlsWalmart = []
         self.titlesWalmart = []
         self.pricesWalmart = []
-        self.HEADERS = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0',
-            'Accept': 'application/json',
-            'Accept-Language': 'en-US,en;q=0.5',
-        })
+        # self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
     def loadList(self, fileName):
-        list_file = open(fileName ,'r')
- 
+        list_file = open(fileName, 'r')
+
         if fileName == "AmazonList.txt":
             self.urlsAmazon = list_file.readlines()
         elif fileName == "WalmartList.txt":
             self.urlsWalmart = list_file.readlines()
-        
+
         list_file.close()
-        
+
     def checkPricesAmazon(self):
         current = 1
         for url in self.urlsAmazon:
             print(f"Currently checking product {current} on Amazon.")
-            page = requests.get(url,headers = self.HEADERS)
-            soup = bs4.BeautifulSoup(page.content,features='lxml')
-            self.titlesAmazon.append(soup.find(id='productTitle').get_text().strip())
+            profile_path = r'/Users/fdonoso/Library/Application Support/Firefox/Profiles/yjau3k6a.FrankieD'
+            service = Service(r'/Users/fdonoso/Downloads/geckodriver')
+            options = Options()
+            options.add_argument("--headless")
+            options.set_preference('profile', profile_path)
+
+            driver = Firefox(service=service, options=options)
+            time.sleep(3)
+            driver.get(url)
+            self.titlesAmazon.append(
+                driver.find_element(By.ID, 'productTitle').text)
 
             try:
                 # print(soup.find(id='sns-base-price').get_text().strip())
-                self.pricesAmazon.append(soup.find('span', class_='a-offscreen').get_text())
+                self.pricesAmazon.append(
+                    driver.find_element(By.ID, 'sns-base-price').text)
                 current = current + 1
-            except:
+
+            except NoSuchElementException:
                 self.pricesAmazon.append(" ")
                 current = current + 1
+
+        # current = 1
+        # for url in self.urlsAmazon:
+        #     print(f"Currently checking product {current} on Amazon.")
+        #     page = requests.get(url, headers=self.HEADERS)
+        #     soup = bs4.BeautifulSoup(page.content, features='lxml')
+        #     self.titlesAmazon.append(soup.find(id='productTitle').get_text().strip())
+        #
+        #     try:
+        #         # print(soup.find(id='sns-base-price').get_text().strip())
+        #         self.pricesAmazon.append(
+        #             soup.find('span', class_='a-offscreen').get_text())
+        #         current = current + 1
+        #     except:
+        #         self.pricesAmazon.append(" ")
+        #         current = current + 1
 
     def checkPricesWalmart(self):
         current = 1
@@ -69,7 +93,8 @@ class PriceChecker:
             try:
                 # print(soup.find(id='sns-base-emprop="price"))
                 # print(page.headers)
-                self.pricesWalmart.append(driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div/div[2]/div/section/main/div[2]/div[2]/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/span[1]/span[2]/span').text)
+                self.pricesWalmart.append(driver.find_element(By.XPATH,
+                                                              '/html/body/div[1]/div[1]/div/div/div[2]/div/section/main/div[2]/div[2]/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/span[1]/span[2]/span').text)
                 driver.quit()
                 current = current + 1
                 continue
@@ -78,38 +103,39 @@ class PriceChecker:
                 # driver.quit()
 
             try:
-                self.pricesWalmart.append(driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div/div[2]/div/section/main/div[2]/div[2]/div/div[1]/div/div/div[1]/div/div/div[2]/div/div/div[1]/span/span[2]/span').text)
+                self.pricesWalmart.append(driver.find_element(By.XPATH,
+                                                              '/html/body/div[1]/div[1]/div/div/div[2]/div/section/main/div[2]/div[2]/div/div[1]/div/div/div[1]/div/div/div[2]/div/div/div[1]/span/span[2]/span').text)
                 current = current + 1
                 driver.quit()
             except:
                 self.pricesWalmart.append("None")
                 current = current + 1
                 driver.quit()
-        
+
     def export(self, strmethod):
         strmethod = 'Placeholder'
 
 
 if __name__ == "__main__":
     t0 = time.time()
-    
+
     checker = PriceChecker()
     # os.chdir("/Users/fdonoso/PycharmProjects/PriceChecker/")
     checker.loadList("AmazonList.txt")
     checker.loadList("WalmartList.txt")
     checker.checkPricesAmazon()
     checker.checkPricesWalmart()
-    
+
     t1 = time.time()
     total = t1 - t0
-    
+
     print('\n')
     print(len(checker.urlsAmazon))
-    print(total/60)
+    print(total / 60)
     print('\n')
 
-    data = list(zip(checker.titlesAmazon,checker.pricesAmazon,checker.pricesWalmart))
+    data = list(zip(checker.titlesAmazon, checker.pricesAmazon, checker.pricesWalmart))
     print("(Item, Amazon Price, Walmart Price)")
-    print(*data,sep ="\n")
+    print(*data, sep="\n")
 
     # print(*checker.titlesAmazon,"-- Amazon Price: ", *checker.pricesAmazon,"-- Walmart Price: ",*checker.pricesWalmart, sep = "\n")
