c60a929682ce9ee00d31bfc8176d3639a21a54ff
fdonoso <fran.donoso1996@gmail.com>
2023-04-10 10:37:52 -0500

    Added exception types

--- checker_multithread.py
+++ checker_multithread.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-
+import os
+import bs4
+import requests
 import time
-
+import threading
 from selenium.common import NoSuchElementException
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 
@@ -19,7 +22,7 @@ class PriceChecker:
         self.urlsWalmart = []
         self.titlesWalmart = []
         self.pricesWalmart = []
-        self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+        # self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
     def loadList(self, fileName):
         list_file = open(fileName, 'r')
@@ -33,49 +36,34 @@ class PriceChecker:
 
     def checkPricesAmazon(self):
         current = 1
-        for url in self.urlsAmazon:
-            print(f"Currently checking product {current} on Amazon.")
-            profile_path = r'/Users/fdonoso/Library/Application Support/Firefox/Profiles/yjau3k6a.FrankieD'
-            service = Service(r'/Users/fdonoso/Downloads/geckodriver')
-            options = Options()
-            options.add_argument("--headless")
-            options.set_preference('profile', profile_path)
-
-            driver = Firefox(service=service, options=options)
-            time.sleep(3)
-            driver.get(url)
-            self.titlesAmazon.append(
-                driver.find_element(By.ID, 'productTitle').text)
-
-            try:
-                # print(soup.find(id='sns-base-price').get_text().strip())
-                self.pricesAmazon.append(
-                    driver.find_element(By.ID, 'sns-base-price').text)
-                current = current + 1
-
-            except NoSuchElementException:
-                self.pricesAmazon.append(" ")
-                current = current + 1
-
-        # current = 1
         # for url in self.urlsAmazon:
         #     print(f"Currently checking product {current} on Amazon.")
-        #     page = requests.get(url, headers=self.HEADERS)
-        #     soup = bs4.BeautifulSoup(page.content, features='lxml')
-        #     self.titlesAmazon.append(soup.find(id='productTitle').get_text().strip())
+        #     profile_path = r'/Users/fdonoso/Library/Application Support/Firefox/Profiles/yjau3k6a.FrankieD'
+        #     service = Service(r'/Users/fdonoso/Downloads/geckodriver')
+        #     options = Options()
+        #     options.add_argument("--headless")
+        #     options.set_preference('profile', profile_path)
+        #
+        #     driver = Firefox(service=service, options=options)
+        #     time.sleep(3)
+        #     driver.get(url)
+        #     self.titlesAmazon.append(
+        #         driver.find_element(By.ID, 'productTitle').text)
         #
         #     try:
         #         # print(soup.find(id='sns-base-price').get_text().strip())
         #         self.pricesAmazon.append(
-        #             soup.find('span', class_='a-offscreen').get_text())
+        #             driver.find_element(By.ID, 'sns-base-price').text)
         #         current = current + 1
-        #     except:
+        #
+        #     except NoSuchElementException:
         #         self.pricesAmazon.append(" ")
         #         current = current + 1
 
     def checkPricesWalmart(self):
         current = 1
         for url in self.urlsWalmart:
+
             print(f"Currently checking product {current} on Walmart.")
             profile_path = r'/Users/fdonoso/Library/Application Support/Firefox/Profiles/yjau3k6a.FrankieD'
             service = Service(r'/Users/fdonoso/Downloads/geckodriver')
@@ -95,7 +83,7 @@ class PriceChecker:
                 driver.quit()
                 current = current + 1
                 continue
-            except NoSuchElementException:
+            except:
                 print("Trying alternative XPATH")
                 # driver.quit()
 
@@ -104,7 +92,7 @@ class PriceChecker:
                                                               '/html/body/div[1]/div[1]/div/div/div[2]/div/section/main/div[2]/div[2]/div/div[1]/div/div/div[1]/div/div/div[2]/div/div/div[1]/span/span[2]/span').text)
                 current = current + 1
                 driver.quit()
-            except NoSuchElementException:
+            except:
                 self.pricesWalmart.append("None")
                 current = current + 1
                 driver.quit()
@@ -114,24 +102,57 @@ class PriceChecker:
 
 
 if __name__ == "__main__":
+
     t0 = time.time()
 
+    threads = []
     checker = PriceChecker()
     # os.chdir("/Users/fdonoso/PycharmProjects/PriceChecker/")
     checker.loadList("AmazonList.txt")
     checker.loadList("WalmartList.txt")
-    checker.checkPricesAmazon()
-    checker.checkPricesWalmart()
+
+    for url in checker.urlsAmazon:
+        t = threading.Thread
+        t.start()
+
+        profile_path = r'/Users/fdonoso/Library/Application Support/Firefox/Profiles/yjau3k6a.FrankieD'
+        service = Service(r'/Users/fdonoso/Downloads/geckodriver')
+        options = Options()
+        options.add_argument("--headless")
+        options.set_preference('profile', profile_path)
+
+        driver = Firefox(service=service, options=options)
+        time.sleep(3)
+        driver.get(url)
+        self.titlesAmazon.append(
+            driver.find_element(By.ID, 'productTitle').text)
+
+        try:
+            # print(soup.find(id='sns-base-price').get_text().strip())
+            self.pricesAmazon.append(
+                driver.find_element(By.ID, 'sns-base-price').text)
+            current = current + 1
+
+        except NoSuchElementException:
+            self.pricesAmazon.append(" ")
+            current = current + 1
+
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+    # checker.checkPricesAmazon()
+    # checker.checkPricesWalmart()
 
     t1 = time.time()
     total = t1 - t0
-
-    print('\n')
-    print(len(checker.urlsAmazon))
-    print(total / 60)
-    print('\n')
-
-    data = list(zip(checker.titlesAmazon, checker.pricesAmazon, checker.pricesWalmart))
-    print("(Item, Amazon Price, Walmart Price)")
-    print(*data, sep="\n")
-    # print(*checker.titlesAmazon,"-- Amazon Price: ", *checker.pricesAmazon,"-- Walmart Price: ",*checker.pricesWalmart, sep = "\n")
+    #
+    # print('\n')
+    # print(len(checker.urlsAmazon))
+    # print(total / 60)
+    # print('\n')
+    #
+    # data = list(zip(checker.titlesAmazon, checker.pricesAmazon, checker.pricesWalmart))
+    # print("(Item, Amazon Price, Walmart Price)")
+    # print(data, sep="\n")
+    # # print(*checker.titlesAmazon,"-- Amazon Price: ", *checker.pricesAmazon,"-- Walmart Price: ",*checker.pricesWalmart, sep = "\n")
